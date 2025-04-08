634710f7cc5eda3e6fceaf9ccbbb27d99d9113f1
Satyam Choubey <163556915+Satyam7371@users.noreply.github.com>
2025-04-09 00:21:07 +0530

    Create LC 31. Next Permutation.java

---
+++ LC 31. Next Permutation.java
@@ -0,0 +1,37 @@
+class Solution {
+    public void reverse(int arr[], int i, int j) {
+
+        while (i <= j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+
+    public void nextPermutation(int[] nums) {
+        int pivot = -1;
+        int n = nums.length;
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                pivot = i;
+                break;
+            }
+        }
+        if (pivot == -1) {
+            reverse(nums, 0, n - 1);
+            return;
+        }
+        for (int i = n - 1; i > pivot; i--) {
+            if (nums[i] > nums[pivot]) {
+                int temp = nums[i];
+                nums[i] = nums[pivot];
+                nums[pivot] = temp;
+                break;
+            }
+        }
+        reverse(nums,pivot+1,n-1);
+
+    }
+}
