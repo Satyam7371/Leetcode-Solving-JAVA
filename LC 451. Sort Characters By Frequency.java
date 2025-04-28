ed01a4bf36a24d24e07af24f3568fae0fe1b0dcb
Satyam Choubey <163556915+Satyam7371@users.noreply.github.com>
2025-04-29 00:42:46 +0530

    Create LC 451. Sort Characters By Frequency.java

--- LC 451. Sort Characters By Frequency.java
+++ LC 451. Sort Characters By Frequency.java
@@ -1,1 +1,18 @@
+class Solution {
+    public String frequencySort(String s) {
+        Map<Character,Integer> mp = new HashMap<>();
 
+        for(int i=0;i<s.length();i++) {
+            mp.put(s.charAt(i),mp.getOrDefault(s.charAt(i),0) + 1);
+        }
+        List<Map.Entry<Character,Integer>> list = new ArrayList<>(mp.entrySet());
+        list.sort((a,b) -> b.getValue() - a.getValue());
+        StringBuilder ans = new StringBuilder();
+        int maxFreq = 0;
+        for(Map.Entry<Character,Integer> entry : list) {
+        
+                ans.append(String.valueOf(entry.getKey()).repeat(entry.getValue()));
+        }
+        return ans.toString();
+    }
+}
