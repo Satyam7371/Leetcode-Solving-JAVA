b1f74713f1ab32cf9070db2930e864bc62ad4fcc
Satyam Choubey <163556915+Satyam7371@users.noreply.github.com>
2025-04-29 10:42:25 +0530

    Create LC 724. Find Pivot Index.java

---
+++ LC 724. Find Pivot Index.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int[] prefixSum = new int[nums.length];
+
+        prefixSum[0] = nums[0];
+        for(int i=1;i<nums.length;i++) {
+            prefixSum[i] = prefixSum[i-1] + nums[i];
+        }
+
+        for(int i=0;i<nums.length;i++) {
+            int leftSum = prefixSum[i] - nums[i];
+            int rightSum = prefixSum[prefixSum.length-1] - prefixSum[i];
+            if(leftSum==rightSum) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
